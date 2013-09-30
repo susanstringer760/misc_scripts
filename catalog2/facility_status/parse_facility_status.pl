729e650047fb4df38bcdb9b7272ce09b3dbe339d
Susan Stringer <snorman@ucar.edu>
2013-09-30 14:21:57 -0600

    added db interactions to parse_facility_status.pl

--- catalog2/facility_status/parse_facility_status.pl
+++ catalog2/facility_status/parse_facility_status.pl
@@ -34,6 +34,7 @@ $category_hash{'aerosols'} = [];
 $category_hash{'irradiance'} = [];
 $category_hash{'chemistry'} = [];
 $category_hash{'oxides_of_nitrogen'} = [];
+$category_hash{'metadata'} = [];
 
 my $project_id = 371;
 my $platform_id = 130;
@@ -42,19 +43,33 @@ my $platform_id = 130;
 
 # get the info from the html file
 my $category_hash_ref = parseHtml($fname,\%category_hash);
+my $metadata_hash = $category_hash_ref->{'metadata'}->[0];
 
 # insert into db
 my $dbh = connectToDB($db_name,$db_user,$db_password);
-print "asdf: $dbh\n";exit();
 
-# now, insert into db
+# now, insert into instrument into db
 foreach $category (keys(%category_hash)) {
-  print "category: $category\n";
+  #print "category: $category\n";
   foreach $instrument (@{$category_hash_ref->{$category}}) {
-    print "\tname: ".$instrument->{'instrument_name'}."\n";
-    print "\tshort name: ".$instrument->{'instrument_short_name'}."\n";
-    print "\tcomment: ".$instrument->{'comment'}."\n";
-    print "***********\n";
+    my $name = $instrument->{'instrument_name'};
+    my $short_name = $instrument->{'instrument_short_name'};
+    my $comment = $instrument->{'comment'};
+    #my $instrument_sql = "insert into instrument (name,short_name) values
+    next() if ( !$name ); 
+    $short_name = '' if ( !$short_name ); 
+    $comment = '' if ( !$comment); 
+
+    # first, make sure it's not a duplicate instrument
+    my $id_sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
+    my $id = $dbh->selectrow_array($id_sql);
+    # don't try to enter duplicate record
+    next() if ( $id ); 
+    my $instrument_sql = "INSERT INTO instrument (name,short_name) VALUES ('$name','$short_name')";
+    $dbh->do($instrument_sql) or die "Couldn't execute sql: $instrument_sql$dbh->errstr";
+    my $id_sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
+    my $instrument_id = $dbh->selectrow_array($id_sql);
+
   }
 }
 
@@ -78,7 +93,7 @@ sub parseHtml
     push(@arr, $_);
   }
   
-  
+  my $report_hash_ref = {};
   for ($i=0; $i<$#arr; $i++) {
   
     $instrument_flag = false;
@@ -86,12 +101,41 @@ sub parseHtml
   
     $line = $arr[$i];
   
+    my $hash_ref = {};
+      #$hash_ref->{'instrument_name'} = $instrument_name;
+      #$hash_ref->{'instrument_short_name'} = $instrument_short_name;
+      #$hash_ref->{'comment'} = $comment;
     # get report metadata
-    $report_date = $arr[$i+1] if ( $line =~ /Date of report/ );
-    $report_author = $arr[$i+1] if ( $line =~ /Author of report/ );
-    $submitted_at = $arr[$i+1] if ( $line =~ /Submitted at/ );
-    $remaining_hours = $arr[$i+1] if ( $line =~ /Remaining flight hours/ );
-    $general_comments = $arr[$i+1] if ( $line =~ /General Comments/ );
+    if ( $line =~ /Date of report/ ) {
+      $report_date = $arr[$i+1];
+      $report_hash_ref->{'report_date'} = $report_date;
+      next();
+    }
+    if ( $line =~ /Author of report/ ) {
+      $report_author = $arr[$i+1]; 
+      $report_hash_ref->{'report_author'} = $report_author;
+      next();
+    }
+    if ( $line =~ /Submitted at/ ) {
+      $submitted_at = $arr[$i+1]; 
+      $report_hash_ref->{'submitted_at'} = $submitted_at;
+      next();
+    }
+    if ( $line =~ /Remaining flight hours/ ) {
+      $remaining_hours = $arr[$i+1]; 
+      $report_hash_ref->{'remaining_hours'} = $remaining_hours;
+      next();
+    }
+    if ( $line =~ /General Comments/ ) {
+      $general_comments = $arr[$i+1]; 
+      $report_hash_ref->{'general_comments'} = $general_comments;
+      next();
+    }
+    #$report_date = $arr[$i+1] if ( $line =~ /Date of report/ );
+    #$report_author = $arr[$i+1] if ( $line =~ /Author of report/ );
+    #$submitted_at = $arr[$i+1] if ( $line =~ /Submitted at/ );
+    #$remaining_hours = $arr[$i+1] if ( $line =~ /Remaining flight hours/ );
+    #$general_comments = $arr[$i+1] if ( $line =~ /General Comments/ );
   
     # get the category information
     $category = 'navigation' if ($line =~ /Navigation/);
@@ -102,16 +146,24 @@ sub parseHtml
     $category = 'chemistry' if ($line =~ /Chemistry/);
     $category = 'oxides_of_nitrogen' if ($line =~ /Oxides of Nitrogen/);
   
-    my $hash_ref = {};
-    if ( $line =~ /<b>\s*([\w\d\-\_\s\(\)\;\,]+)\s*<\/b>/ ) {
+    #my $hash_ref = {};
+    if ($line =~ /<b>(.*)(\(.*)<\/b>/) {  
       $instrument_name = $1;
-      $instrument_name =~ /(.*)\s*\(([\w\d\-\_\s]+)\)*/;
+      #$instrument_name =~ /(.*)\s*\(([\w\d\-\_\s]+)\)*/;
       $instrument_short_name = $2;
       $comment = $arr[$i+4];
       # get rid of any leading spaces
       $instrument_name =~ s/^(\s*)(.*)/$2/;
       $instrument_short_name =~ s/^(\s*)(.*)/$2/;
       $comment =~ s/^(\s*)(.*)/$2/;
+      # remove trailing spaces
+      $instrument_name =~ s/\s+$//g;
+      $instrument_short_name =~ s/\s+$//g;
+      $comment =~ s/\s+$//g;
+
+      # remove () from short_name
+      $instrument_short_name =~ s/\(|\)//g;
+
       $hash_ref->{'instrument_name'} = $instrument_name;
       $hash_ref->{'instrument_short_name'} = $instrument_short_name;
       $hash_ref->{'comment'} = $comment;
@@ -123,6 +175,9 @@ sub parseHtml
     }
   
   }
+
+  # all the report global information
+  push(@{$category_hash_ref->{'metadata'}}, $report_hash_ref);
 
   return $category_hash_ref;
   
