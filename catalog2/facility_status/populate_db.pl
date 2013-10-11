e9c349bf57a4fcfdde2fb65c05d66050d86cb26b
Susan Stringer <snorman@ucar.edu>
2013-10-11 09:33:29 -0600

    added command line options to populate_db.pl...not yet working

--- catalog2/facility_status/populate_db.pl
+++ catalog2/facility_status/populate_db.pl
@@ -3,149 +3,155 @@
 # create fake instruments and add to
 # zith9_mpex_facility_status db
 
+use Getopt::Std;
 use DBI;
 
-my $gv_platform_id = 350;
-my $c130_platform_id = 130;
+if ( $#ARGV < 0 ) {
+  print "$0:\n";
+  print "\t-n: db name\n";
+  print "\t-u: db user\n";
+  print "\t-p: db password\n";
+  print "\t-h: db host\n";
+  print "\t-d: date\n";
+  exit();
+}
 
-#my %platform_hash;
-my $platform_ref = {};
+# db information
+getopt('nuphd');
+my $db_name = $opt_n;
+my $db_user = $opt_u;
+my $db_password = $opt_p;
+my $db_host = $opt_h;
+my $date = $opt_d;
+my $dbh = connectToDB($db_name,$db_user,$db_password,$db_host);
+
+my $project_id=371;
+my %platform_id;
+$platform_id{'gv'} = 350;
+$platform_id{'c130'} = 130;
+$platform_id{'goes_13'} = 405;
+$platform_id{'goes_10'} = 232;
+$platform_id{'csu_chill'} = 450;
+$platform_id{'spol'} = 398;
+$platform_id{'wsr_88d'} = 79;
+$platform_id{'aws'} = 88;
+$platform_id{'gps'} = 150;
+
+my %category_id;
+$category_id{'gv'} = 2;
+$category_id{'c130'} = 2;
+$category_id{'goes_13'} = 15;
+$category_id{'goes_10'} = 15;
+$category_id{'csu_chill'} = 14;
+$category_id{'spol'} = 14;
+$category_id{'wsr_88d'} = 14;
+$category_id{'aws'} = 19;
+$category_id{'gps'} = 19;
+
+my $facility_status_ref = {};
 my $sql;
 
-# GV
-my @gv_instrument_arr = (1 .. 15);
-@gv_instrument_arr = map("gv_instrument$_", @gv_instrument_arr);
-my @gv_instrument_values;
-for ($i=0; $i<=$#gv_instrument_arr;$i++) {
-  $instrument = $gv_instrument_arr[$i];
-  if ( $i >= 0 && $i < 5 ) {
-    $category_id = 58,
-  } elsif ( $i >= 5 && $i < 12 ) {
-    $category_id = 59,
-  } else {
-    $category_id = 60,
-  }
-  my $ref = {
-    'instrument_name'=>$instrument,
-    'category_id'=>$category_id,
-    'platform_id'=>$gv_platform_id
-  };
-  push(@gv_instrument_values, $ref);
+#xx# GV
+#xxmy @gv_instrument_arr = (1 .. 15);
+#xx@gv_instrument_arr = map("gv_instrument$_", @gv_instrument_arr);
+#xxmy @gv_instrument_values;
+#xxmy %gv_instrument_category_id;
+#xxmy $facility_status_category_id;
+#xxmy $facility_status_status;
+#xxfor ($i=0; $i<=$#gv_instrument_arr;$i++) {
+#xx  my $name = $gv_instrument_arr[$i];
+#xx  my $short_name = $name;
+#xx  $short_name =~ s/instrument//g;
+#xx  my $facility_status_comment = "$name comment";
+#xx  if ( $i >= 0 && $i < 5 ) {
+#xx    $facility_status_category_id = 58,
+#xx    $facility_status_status = 'up';
+#xx  } elsif ( $i >= 5 && $i < 12 ) {
+#xx    $facility_status_category_id = 59,
+#xx    $facility_status_status = 'down';
+#xx  } else {
+#xx    $facility_status_category_id = 60,
+#xx    $facility_status_status = 'provisional';
+#xx  }
+#xx  # create instrument
+#xx  my $instrument_id = insert_instrument($dbh,$name,$short_name);
+#xx
+#xx  my $hash = {
+#xx    'project_id'=>$project_id,
+#xx    'platform_id'=>$platform_id{'gv'},
+#xx    'instrument_id'=>$instrument_id,
+#xx    'category_id'=>$facility_status_category_id,
+#xx    'status'=>$facility_status_status,
+#xx    'comment'=>$facility_status_comment,
+#xx  };
+#xx
+#xx  my $facility_status_id = insert_facility_status($dbh,$hash,$name);
+#xx
+#xx  print "asdf: $name and $short_name and $instrument_id\n";
+#xx  exit();
+#xx
+#xx}
+#xx
+#************************
+sub insert_facility_status {
+
+  my $dbh = shift;
+  my $column_value_hash = shift;
+  print "testit: $id\n";exit();
+
+  # assemble the sql
+
 }
-print "GV\n";
-foreach $hash (@gv_instrument_values) {
-  print $hash->{'instrument_name'}."\n";
-  $sql = "insert into instrument (instrument_name) values ('$instrument')";
-  print "\tcategory id: ".$hash->{'category_id'}."\n";
-  print "\tplatform id: ".$hash->{'platform_id'}."\n";
-  $sql = "insert
-}
-print "**************\n";
-
-# C130
-my @c130_instrument_arr = (1 .. 20);
-@c130_instrument_arr = map("c130_instrument$_", @c130_instrument_arr);
-my @c130_instrument_values;
-for ($i=0; $i<=$#c130_instrument_arr;$i++) {
-  $instrument = $c130_instrument_arr[$i];
-  $platform_id = 130;
-  if ( $i >= 0 && $i < 9 ) {
-    $category_id = 58,
-  } elsif ( $i >= 9 && $i < 15 ) {
-    $category_id = 59,
-  } else {
-    $category_id = 60,
-  }
-  my $ref = {
-    'instrument_name'=>$instrument,
-    'category_id'=>$category_id,
-    'platform_id'=>$c130_platform_id,
-  };
-  push(@c130_instrument_values, $ref);
-}
-
-print "C130\n";
-foreach $hash (@c130_instrument_values) {
-  print $hash->{'instrument_name'}."\n";
-  print "\tcategory id: ".$hash->{'category_id'}."\n";
-  print "\tplatform id: ".$hash->{'platform_id'}."\n";
-}
-print "**************\n";
-
-exit();
-
-
-my $platform1_arr_ref = [];
-for ($i=0;$i<=$#instrument1_arr;$i++) {
-  $instrument = $instrument1_arr[$i];
-  if ( $i < 7 ) {
-    $platform = "platform1";
-  } else {
-    $platform = "platform2";
-  }
-  print "$instrument and $platform\n";
-}
-exit();
-$platform_arr_ref->[0] = {
-  'platform'=>'platform1',
-  'instrument'=>'instrument1',
-  'category_id'=>11
-};
-print "asdfasdf: ".$platform_arr_ref->[0]->{'platform'}."\n";
-print "zxcvzxcv: ".$platform_arr_ref->[0]->{'instrument'}."\n";
-print "asdfasdf: ".$platform_arr_ref->[0]->{'category_id'}."\n";
-exit();
-
-
-$platform_hash{'platform1'} = \@instrument1_arr;
-$platform_hash{'platform2'} = \@instrument2_arr;
-$platform_hash{'platform3'} = \@instrument3_arr;
-$platform_hash{'platform4'} = \@instrument4_arr;
-
-foreach $platform (sort(keys(%platform_hash))) {
-  print "$platform\n";
-  my $arr_ref = $platform_hash{$platform};
-  foreach $instrument (@$arr_ref) {
-    print "\t$instrument\n";
-  }
-}
-
-my $name = $instrument->{'instrument_name'};
-my $short_name = $instrument->{'instrument_short_name'};
-my $comment = $instrument->{'comment'};
-next() if ( !$name );
-$short_name = '' if ( !$short_name );
-$comment = '' if ( !$comment);
-
-#first, make sure it's not a duplicate instrument
-my $id_sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
-my $id = $dbh->selectrow_array($id_sql);
-#don't try to enter duplicate record
-next() if ( $id );
     
-
 sub insert_instrument {
 
+  my $dbh = shift;
   my $name = shift;
-  my $short_name = '';
+  my $short_name = shift;
 
   # first, make sure it's not a duplicate instrument
+  my $instrument_id = get_instrument_id($dbh,$name,$short_name);
   my $id_sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
   my $id = $dbh->selectrow_array($id_sql);
   # don't try to enter duplicate record
   next() if ( $id );
   my $sql = "INSERT INTO instrument (name,short_name) VALUES ('$name','$short_name')";
   print "$sql\n";
+  $dbh->do($sql) or die "Couldn't execute sql: $instrument_sql$dbh->errstr";
+  $instrument_id = get_instrument_id($dbh,$name,$short_name);
+
+  return $instrument_id;
+
+}
+
+sub get_facility_status_id {
+
+  my $dbh = shift;
+  my $project_id = shift;
+  my $platform_id = shift;
+  my $status = shift;
+  my $comment = shift;
+  my $report_date = shift;
+
+sub insert_facility_status {
+
+  my $sql = "select id from catalog_facility_status where (project_id = $project_id) and (platform_id = $platform_id) and (status='$status') and (comment='$comment') and (report_date='$report_date')";
+  print "$sql\n";
   exit();
-  #$dbh->do($instrument_sql) or die "Couldn't execute sql: $instrument_sql$dbh->errstr";
+  
+ # my $instrument_id = $dbh->selectrow_array($sql);
+
+ # return $instrument_id;
 
 }
 sub get_instrument_id {
 
+  my $dbh = shift;
   my $name = shift;
-  my $short_name = '';
-  my $id_sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
-  my $instrument_id = $dbh->selectrow_array($id_sql);
+  my $short_name = shift;
+  my $sql = "SELECT id FROM instrument WHERE name='$name' AND short_name='$short_name'";
+  print "$sql\n";
+  my $instrument_id = $dbh->selectrow_array($sql);
 
   return $instrument_id;
 
@@ -168,3 +174,33 @@ sub insert_facility_status {
   $dbh->do($instrument_sql) or die "Couldn't execute sql: $instrument_sql$dbh->errstr";
 
 }
+sub connectToDB()
+{
+
+  my $db_name = shift;
+  my $user = shift;
+  my $password = shift;
+  my $host = shift;
+
+  return DBI->connect( "DBI:mysql:database=$db_name;
+                       host=$host",
+                       "$user",
+                       "$password",
+                       { PrintError => 0,
+                         PrintWarn => 1,
+                         RaiseError => 1,
+                         HandleError => \&dbiErrorHandler,
+                       } ) || die( "Unable to Connect to database" );
+
+}
+
+sub dbiErrorHandler {
+
+  $error = shift;
+  print "ERROR: $error\n";
+  exit();
+
+  return 1;
+
+}
+
