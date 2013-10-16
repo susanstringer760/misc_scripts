9f1c5abd91e48378569c3dc334a3ab618b326677
Susan Stringer <snorman@ucar.edu>
2013-10-15 19:58:22 -0600

    run_populate_db.pl: add multiple days

--- catalog2/facility_status/run_populate_db.pl
+++ catalog2/facility_status/run_populate_db.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 #
 
-foreach my $i (0..7)
+# script to populate database for 40 days
+
+foreach my $i (1..40)
 {
   #my $date = localtime(time() - 60*60*24*$i);
   #my ($sec,$min,$hour,$day,$month,$year) = (localtime(time))[0,1,2,3,4,5];
@@ -9,15 +11,10 @@ foreach my $i (0..7)
   my $time = time;
   $time += $diff;
   my ($sec,$min,$hour,$day,$month,$year) = (localtime($time))[0,1,2,3,4,5];
-  #my $yy = $xx + $diff;
-  #print "xx: $xx\n";
-  #print "yy: $yy\n\n";
-  #next();
   my $report_date = sprintf ('%4d-%02d-%02d %02d:%02d:%02d', $year+1900,$month+1, $day,$hour,$min,$sec) ;
 
   my $script_fname = "/usr/local/snorman/misc_scripts/catalog2/facility_status/populate_db.pl";
   $script_fname = "$script_fname -n zith9_mpex_facility_status -u snorman -p emdac -h localhost -d '$report_date'";
   print "$script_fname\n";
-  #system($script_fname);
-  exit();
+  system($script_fname);
 }
