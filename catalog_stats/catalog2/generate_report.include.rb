799f2f38f9bdff57a6181c546ef4ac185c3b9c93
Susan Stringer <snorman@ucar.edu>
2014-01-24 08:36:26 -0700

    added command line options to generate_report.rb

--- catalog_stats/catalog2/generate_report.include.rb
+++ catalog_stats/catalog2/generate_report.include.rb
@@ -12,8 +12,26 @@ def load_models
 
 end
 
-def get_categories
+def get_datasets(project_id)
 
-  Category.find(28)
+  Project.find(project_id).datasets.catalog_viewable
+
+  # a list of catalog dataset for this project
+  #Dataset.catalog_viewable.find(:all, :conditions => ["archive_ident LIKE ?", "#{project_id}.fc%"])
+
+end
+
+def get_categories(project_id)
+
+  # get the available categories for the project
+  dataset_arr = get_datasets(project_id)
+  
+  # arr of categories for the datasets
+  category_arr = dataset_arr.map{|dataset| dataset.categories}.flatten
+  category_hash = Hash.new
+
+  # return hash where key is category short_name and value is category id 
+  category_arr.map{|category| category_hash[category.short_name] = category.id}
+  category_hash
 
 end
--- catalog_stats/catalog2/generate_report.rb
+++ catalog_stats/catalog2/generate_report.rb
@@ -1,16 +1,48 @@
 #!/bin/env ruby
 #
+
+require 'optparse'
 require "rubygems"
 require 'mysql2'
 require "active_record"
 require File.join(File.dirname(__FILE__), 'generate_report.include.rb')
 
-# generate the field catalog stats
-#
-# load catalog models
+if ( ARGV.length==0 )
+  puts "USAGE: generate_report.pl -p [project_id]"
+  exit
+end
+
+options = {:project_id => nil}
+
+parser = OptionParser.new do|opts|
+	opts.banner = "Usage: generate_report.rb [options]"
+	opts.on('-n', '--project_id project_id', 'project id') do |project_id|
+		options[:project_id] = project_id;
+	end
+end
+
+parser.parse!
+
+if options[:project_id] == nil
+	print 'Enter Name: '
+    options[:project_id] = gets.chomp
+end
+
+project_id = options[:project_id]
+
+# load the models
 load_models()
+project_id = 382
 
-xx = get_categories()
+# all the catalog datasets for this project
+project_datasets = get_datasets(project_id)
 
-#xx = Category.find(28)
-puts "#{xx.class}"
+category_hash = Hash.new{|hash, key| hash[key] = Array.new}
+project_datasets.each {|d| 
+  category = d.categories[0]
+  category_hash[category.short_name].push(d) 
+}
+
+category_hash.each_key {|category|
+  puts "#{category}: #{category_hash[category].length}"
+}
